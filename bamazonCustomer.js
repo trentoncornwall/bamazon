2186b8f4cec75922932bee40ea9b07bd1f7dea50
Trenton Cornwall <trenton434@gmail.com>
2019-09-08 15:49:30 -0600

    customer section complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,12 +11,13 @@ const connection = mysql.createConnection({
 	database: "bamazon_DB"
 });
 
+let inventory = [];
+
 //* Initial connection *//
 connection.connect(err => {
 	if (err) throw err;
-	selectAlL();
 	mainMenu();
-	connection.end();
+	selectAll();
 });
 
 //* Inquirer Menus *//
@@ -71,11 +72,34 @@ function customer() {
 			}
 		])
 		.then(response => {
-			console.log(`You'd like to by: ${response.itemid}`);
+			resItem = parseInt(response.itemid, 10);
+			resAmount = parseInt(response.amount, 10);
+			inventory.forEach(element => {
+				if (element.item_id === resItem) {
+					if (resAmount <= element.stock_quantity) {
+						purchaseCost = element.price * resAmount;
+						newAmount = element.stock_quantity - resAmount;
+						return buy(resItem, newAmount, purchaseCost);
+					} else {
+						console.log(`\nInsufficient quantity!\n`);
+					}
+				}
+			});
 		});
 }
 
+function buy(item, newAmount, purchaseCost) {
+	connection.query(
+		`UPDATE products SET stock_quantity=${newAmount} WHERE item_id=${item}`,
+		(err, res) => {
+			if (err) throw err;
+			console.log(`\nTotal Cost: $ ${purchaseCost}\n`);
+		}
+	);
+}
+
 function printNeat(x) {
+	console.log("\n\n");
 	console.log(
 		columnify(x, {
 			config: { item_id: { align: "right" } },
@@ -89,11 +113,48 @@ function printNeat(x) {
 			]
 		})
 	);
+	console.log("\n\n");
 }
 
 function selectAll() {
 	connection.query("SELECT * FROM products", (err, res) => {
 		if (err) throw err;
+		inventory = [];
+		function PopInv(
+			item_id,
+			product_name,
+			department_name,
+			price,
+			stock_quantity
+		) {
+			this.item_id = item_id;
+			this.product_name = product_name;
+			this.department_name = department_name;
+			this.price = price;
+			this.stock_quantity = stock_quantity;
+
+			this.pushInv = function() {
+				inventory.push({
+					item_id: this.item_id,
+					product_name: this.product_name,
+					department_name: this.department_name,
+					price: this.price,
+					stock_quantity: this.stock_quantity
+				});
+			};
+		}
+
+		res.forEach(element => {
+			var ele = new PopInv(
+				element.item_id,
+				element.product_name,
+				element.department_name,
+				element.price,
+				element.stock_quantity
+			);
+			ele.pushInv();
+		});
+
 		printNeat(res);
 	});
 }
